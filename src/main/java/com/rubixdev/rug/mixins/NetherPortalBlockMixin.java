4fd7e7f83f9f97cd978e2d4dac52a836849f8e7b
RubixDev <silas.groh@t-online.de>
2021-02-17 20:24:27 +0100

    Changed Overwrite to Inject

--- src/main/java/com/rubixdev/rug/mixins/NetherPortalBlockMixin.java
+++ src/main/java/com/rubixdev/rug/mixins/NetherPortalBlockMixin.java
@@ -1,47 +1,23 @@
 package com.rubixdev.rug.mixins;
 
 import com.rubixdev.rug.RugSettings;
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.NetherPortalBlock;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.SpawnReason;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.text.Text;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.GameRules;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Random;
 
 @Mixin({NetherPortalBlock.class})
-public class NetherPortalBlockMixin extends Block {
-
-    public NetherPortalBlockMixin(Settings settings) {
-        super(settings);
-    }
-
-    /**
-     * @author RubixDev
-     */
-    @Overwrite
-    public void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
-        if (RugSettings.zombifiedPiglinsSpawningInPortals && world.getDimension().isNatural() && world.getGameRules().getBoolean(GameRules.DO_MOB_SPAWNING) && random.nextInt(2000) < world.getDifficulty().getId()) {
-            while(world.getBlockState(pos).isOf(this)) {
-                pos = pos.down();
-            }
-
-            if (world.getBlockState(pos).allowsSpawning(world, pos, EntityType.ZOMBIFIED_PIGLIN)) {
-                Entity entity = EntityType.ZOMBIFIED_PIGLIN.spawn(world, (CompoundTag)null, (Text)null, (PlayerEntity)null, pos.up(), SpawnReason.STRUCTURE, false, false);
-                if (entity != null) {
-                    entity.resetNetherPortalCooldown();
-                }
-            }
+public class NetherPortalBlockMixin {
+    @Inject(method = "randomTick", at = @At("HEAD"), cancellable = true)
+    private void onRandomTick(BlockState state, ServerWorld world, BlockPos pos, Random random, CallbackInfo ci) {
+        if (!RugSettings.zombifiedPiglinsSpawningInPortals) {
+            ci.cancel();
         }
-
     }
 }
