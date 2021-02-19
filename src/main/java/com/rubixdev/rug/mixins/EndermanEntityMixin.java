a65732fab0de82244e8b7965f97c0be38bd6c076
RubixDev <silas.groh@t-online.de>
2021-02-19 10:36:41 +0100

    Fixed build error

--- src/main/java/com/rubixdev/rug/mixins/EndermanEntityMixin.java
+++ src/main/java/com/rubixdev/rug/mixins/EndermanEntityMixin.java
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(targets = {"net.minecraft.entity.mob.EndermanEntity$PickUpBlockGoal", "net.minecraft.entity.mob.EndermanEntity$PlaceBlockGoal"})
 public class EndermanEntityMixin {
-    @Inject(method = "canStart", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "canStart()Z", at = @At("HEAD"), cancellable = true)
     private void onPickUpCanStart(CallbackInfoReturnable<Boolean> cir) {
         if (RugSettings.noEndermanGriefing) {
             cir.setReturnValue(false);
