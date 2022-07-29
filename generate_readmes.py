786e94dea9a4f619cb5b5e36dab14906a7267961
RubixDev <silas.groh@t-online.de>
2022-07-29 11:53:58 +0200

    Fix python script

--- generate_readmes.py
+++ generate_readmes.py
@@ -7,6 +7,32 @@ import json
 with open('pyconfig.json', 'r') as config_file:
     SETTINGS: dict[str, any] = json.load(config_file)
 
+with open('src/main/resources/assets/rug/lang/en_us.json5', 'r') as lang_file:
+    # Remove lines with comments
+    contents: str = re.compile(
+        r'^\s*//.*$', re.MULTILINE).sub('', lang_file.read())
+    descriptions_raw: dict[str, any] = json.loads(contents)
+# DESCRIPTIONS has following structure:
+# { "ruleName": { "desc": "...", "extra": ["...", "..."] } }
+DESCRIPTIONS: dict[str, dict] = {}
+for k, v in descriptions_raw.items():
+    rule_name = re.compile(
+        r'carpet\.rule\.(\w+)\.(?:desc|extra\.\d+)').search(k).group(1)
+    if rule_name not in DESCRIPTIONS.keys():
+        DESCRIPTIONS[rule_name] = {
+            'desc': '',
+            'extra': [],
+        }
+    if k.endswith('.desc'):
+        DESCRIPTIONS[rule_name]['desc'] = v
+    elif re.compile(r'\.extra\.\d+$').search(k):
+        index = int(k.split('.')[-1])
+        while len(DESCRIPTIONS[rule_name]['extra']) <= index:
+            DESCRIPTIONS[rule_name]['extra'].append('')
+        DESCRIPTIONS[rule_name]['extra'][index] = v
+    else:
+        print(f'\x1b[1;33mWarning: unknown key {k} in language file\x1b[0m')
+
 
 class Rule:
     type: str
@@ -22,7 +48,8 @@ class Rule:
 
     def __repr__(self):
         nl: str = '\n'
-        options: list[str] = ['true', 'false'] if self.type == 'boolean' else self.options
+        options: list[str] = [
+            'true', 'false'] if self.type == 'boolean' else self.options
         if 'COMMAND' in self.categories:
             options: list[str] = ['true', 'false', 'ops']
 
@@ -50,44 +77,49 @@ def read_rules() -> list[Rule]:
     with open(SETTINGS['carpetSettingsClass'], 'r') as settings_file:
         print('Reading settings file\n')
         settings_string = settings_file.read()
-    raw_rules: list[str] = [i.split(';')[0] for i in re.compile('^[^/\n]*[^/\n]*@Rule', re.MULTILINE).split(settings_string)[1:]]
+    raw_rules: list[str] = [i.split(';')[0] for i in re.compile(
+        '^[^/\n]*[^/\n]*@Rule', re.MULTILINE).split(settings_string)[1:]]
 
     rules: list[Rule] = []
     for raw_rule in raw_rules:
         rule: Rule = Rule()
-        field: list[str] = raw_rule.split('\n')[-1].split('public static ')[-1].split(' ')
+        field: list[str] = raw_rule.split(
+            '\n')[-1].split('public static ')[-1].split(' ')
         rule.type = field[0]
         rule.name = field[1]
         print(f'Parsing rule {rule.name}')
         rule.value = field[3].replace('"', '')
 
-        attr_dict: dict[str: str] = { match.group(1): match.group(2) for match in re.finditer(r'(name|desc|extra|options|category|strict|appSource|validate) = ([\s\S]+?)(?=,\n?\s*?\w+?\s?=\s?|\n?\s*?\)\n)', raw_rule) }
+        attr_dict: dict[str: str] = {
+            match.group(1): match.group(2) for match in re.finditer(
+                r'(options|categories|strict|appSource|validators|conditions) = ([\s\S]+?)(?=,\n?\s*?\w+?\s?=\s?|\n?\s*?\)\n)',
+                raw_rule
+            )
+        }
 
-        rule.desc = attr_dict['desc'][1:-1]
-        if 'extra' in attr_dict.keys():
-            if (attr_dict['extra'][0] == '{'):
-                rule.extra = [
-                    re.sub(r'([^\\]|^)"', r'\1|||||', extra)
-                        .split('|||||')[1].split('|||||')[0]
-                        .replace('\\"', '"')
-                    for extra in re.sub(r'",\s|",\n', '"|||||', attr_dict['extra'][1:-1]).split('|||||')
-                ]
-            else:
-                rule.extra = [attr_dict['extra'][1:-1]]
+        rule.desc = DESCRIPTIONS[rule.name]['desc']
+        rule.extra = DESCRIPTIONS[rule.name]['extra']
         if 'options' in attr_dict.keys():
-            rule.options = [re.sub(r'\s|\n', '', option)[1:-1] for option in re.compile(r',\s|,\n').split(attr_dict['options'][1:-1])]
-        rule.strict = not ('strict' in attr_dict.keys()) or attr_dict['strict'] == 'true'
-        rule.categories = [category for category in attr_dict['category'][1:-1].replace(' ', '').split(',')]
+            rule.options = [re.sub(r'\s|\n', '', option)[
+                1:-1] for option in re.compile(r',\s|,\n').split(attr_dict['options'][1:-1])]
+        rule.strict = not ('strict' in attr_dict.keys()
+                           ) or attr_dict['strict'] == 'true'
+        rule.categories = [
+            category for category in attr_dict['categories'][1:-1].replace(' ', '').split(',')]
         main_category: str = SETTINGS['mainCategory']
         if main_category not in rule.categories:
-            print(f'\033[1;31m{main_category} category is missing in {rule.name}!\033[22m Exiting...\033[0m')
+            print(
+                f'\033[1;31m{main_category} category is missing in {rule.name}!\033[22m Exiting...\033[0m')
             return []
-        if 'validate' in attr_dict.keys():
-            validator: str = attr_dict['validate'].replace('.class', '')
-            rule.restriction = settings_string.split(f'class {validator} extends')[1].split('"')[1]
-        found_additional: list[str] = settings_string.split(f'// {rule.name}Additional: ')
+        if 'validators' in attr_dict.keys():
+            validator: str = attr_dict['validators'].replace('.class', '')
+            rule.restriction = settings_string.split(
+                f'class {validator} extends')[1].split('"')[1]
+        found_additional: list[str] = settings_string.split(
+            f'// {rule.name}Additional: ')
         if len(found_additional) > 1:
-            rule.additional = re.sub(r'\n\s+?//\s?', ' ', found_additional[1].split(':::')[0])
+            rule.additional = re.sub(
+                r'\n\s+?//\s?', ' ', found_additional[1].split(':::')[0])
 
         rules.append(rule)
         print(f'Successfully parsed {rule.name}')
@@ -101,11 +133,13 @@ def write_files(rules: list[Rule]):
         out: str = header_file.read()
 
     print('Listing all categories')
-    all_categories: list[str] = list(set([item for sublist in [rule.categories for rule in rules] for item in sublist]))
-    all_categories = [category for category in all_categories if category.upper() != SETTINGS['mainCategory']]
+    all_categories: list[str] = list(set(
+        [item for sublist in [rule.categories for rule in rules] for item in sublist]))
+    all_categories = [category for category in all_categories if category.upper(
+    ) != SETTINGS['mainCategory']]
     all_categories.sort()
 
-    out += f'## Lists of Categories\n'
+    out += '## Lists of Categories\n'
     for category in all_categories:
         out += f'- [`{category}`](markdown/{category}_Category.md)\n'
     out += '\n'
@@ -121,7 +155,8 @@ def write_files(rules: list[Rule]):
 
     for category in all_categories:
         print(f'Listing rules in {category} category')
-        rules_in_category: list[Rule] = [rule for rule in rules if category in rule.categories]
+        rules_in_category: list[Rule] = [
+            rule for rule in rules if category in rule.categories]
         rules_in_category.sort(key=lambda e: e.name)
         out: str = f'# List of Rules in the {category} Category\n\n' \
                    f'For a list of all implemented Rules go [here](../README.md)\n'
