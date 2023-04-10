c6dd73337003da89e1d231fcc75162b467598208
RubixDev <silas.groh@t-online.de>
2023-04-10 21:05:44 +0200

    fix: `world is null` when using baritone

--- src/main/java/de/rubixdev/rug/mixins/AbsractBlockStateMixin.java
+++ src/main/java/de/rubixdev/rug/mixins/AbsractBlockStateMixin.java
@@ -2,22 +2,27 @@ package de.rubixdev.rug.mixins;
 
 import de.rubixdev.rug.RugSettings;
 import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.BlockView;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(AbstractBlock.AbstractBlockState.class)
-public class AbsractBlockStateMixin {
+public abstract class AbsractBlockStateMixin {
+    @Shadow
+    public abstract Block getBlock();
+
     @Inject(
             method = "getHardness(Lnet/minecraft/world/BlockView;Lnet/minecraft/util/math/BlockPos;)F",
             at = @At("HEAD"),
             cancellable = true)
     private void onGetHardness(BlockView world, BlockPos pos, CallbackInfoReturnable<Float> cir) {
-        if (world.getBlockState(pos).isOf(Blocks.DRIED_KELP_BLOCK)) {
+        if (this.getBlock() == Blocks.DRIED_KELP_BLOCK) {
             cir.setReturnValue((float) RugSettings.kelpBlockHardness);
         }
     }
