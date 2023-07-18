d9e0922282674b5d29a8e5c8b3ee56bb11cc4326
RubixDev <silas.groh@t-online.de>
2023-07-18 10:17:34 +0800

    fix: wrong version string

--- src/main/java/de/rubixdev/rug/RugServer.java
+++ src/main/java/de/rubixdev/rug/RugServer.java
@@ -55,7 +55,7 @@ import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.Nullable;
 
 public class RugServer implements CarpetExtension, ModInitializer {
-    public static final String VERSION = "1.3.7";
+    public static final String VERSION = "1.4.2";
     public static final Logger LOGGER = LogManager.getLogger("Rug");
 
     private static MinecraftServer minecraftServer;
